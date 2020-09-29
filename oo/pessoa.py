e83c3c455e3993dddc3632d5dc2cd50943d129de
LuandoAV <70849901+LuandoAV@users.noreply.github.com>
2020-09-29 13:47:40 -0300

    Criada a classe pessoa.py - Inclusão do metodo cumprimentar - Objeto de dados

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,9 +1,16 @@
 class Pessoa:
+    def __init__(self, nome=None, idade=35):
+        self.nome = nome
+        self.idade = idade
+
     def cumprimentar(self):
         return f'OLá {id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa('Luciano')
     print(Pessoa.cumprimentar(p))
     print(id(p))
     print(p.cumprimentar())
+    p.nome = 'Luando'
+    print(p.nome)
+    print(p.idade)
